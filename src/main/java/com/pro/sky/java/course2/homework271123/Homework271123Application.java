36a4eb5aa422db2471bc32866f96f555e3d90d1a
miksor2023 <miksor@yandex.ru>
2023-11-29 10:25:58 +0300

    initial commit

---
+++ src/main/java/com/pro/sky/java/course2/homework271123/Homework271123Application.java
@@ -0,0 +1,13 @@
+package com.pro.sky.java.course2.homework271123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework271123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework271123Application.class, args);
+	}
+
+}
